8a88feae6ae71b874832fc6ae4f150a5615000b1
mind1949 <lianjie1949@gmail.com>
2023-03-26 13:24:23 +0800

    complete ch03

---
+++ README.md
@@ -0,0 +1,6 @@
+# Practical System Programming for Rust Developers
+学习 [Practical System Programming for Rust Developers](https://www.amazon.com/Practical-System-Programming-Rust-Developers-ebook/dp/B08MBCQ5L1/ref=sr_1_8?crid=17LVG7MNBX4U3&keywords=rust&qid=1679118630&s=books&sprefix=rust%2Cstripbooks-intl-ship%2C1790&sr=1-8)
+
+# references
+- [book source code](https://github.com/PacktPublishing/Practical-System-Programming-for-Rust-Developers)
+
---
+++ ch03/template-gengine/src/main.rs
@@ -0,0 +1,31 @@
+use std::collections::HashMap;
+use std::io;
+use std::io::BufRead;
+use template_engine::*;
+
+fn main() {
+    let mut context: HashMap<String, String> = HashMap::new();
+    context.insert("name".to_string(), "Bob".to_string());
+    context.insert("city".to_string(), "boston".to_string());
+
+    for line in io::stdin().lock().lines() {
+        match get_content_type(line.unwrap().clone()) {
+            ContentType::TemplateVariable(content) => {
+                let html = generate_html_template_var(content, context.clone());
+                println!("{}", html);
+            }
+            ContentType::Literal(text) => {
+                println!("{}", text);
+            }
+            ContentType::Tag(TagType::ForTag) => {
+                println!("For tag not implemented");
+            }
+            ContentType::Tag(TagType::IfTag) => {
+                println!("If tag not implemented");
+            }
+            ContentType::Unrecognized => {
+                println!("Unrecognized input")
+            }
+        }
+    }
+}
